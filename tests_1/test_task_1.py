a51faf3cc009ac8e19e7f8d27f33a406264c5469
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 12:22:28 -0700

    Create test_task_1.py

---
+++ tests_1/test_task_1.py
@@ -0,0 +1,39 @@
+import subprocess
+import task_1
+
+
+def test_exponentiation_result():
+    # Import variables from the task script
+    a = task_1.a
+    b = task_1.b
+    result = task_1.result
+
+    # Check the result
+    expected_result = a**b
+
+    assert (
+        result == expected_result
+    ), f"Expected result to be {expected_result} but got {result}"
+
+
+def test_printed_output():
+    # Import result variable from the task script
+    result = task_1.result
+
+    # Run the task script
+    console_out = subprocess.run(
+        ["python", "./homework/task_1.py"], capture_output=True, text=True
+    )
+
+    # Check the printed output
+    expected_output = (
+        f"Result: {result}\n"  # Adjust based on example values in the task script
+    )
+    assert (
+        console_out.stdout == expected_output
+    ), f"Expected '{expected_output}' but got '{console_out.stdout}'"
+
+
+if __name__ == "__main__":
+    test_exponentiation_result()
+    test_printed_output()
