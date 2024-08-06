d5d937a9d3a83ae82340e182c5aa87d7c6e2b297
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 12:23:25 -0700

    Create test_task_3.py

---
+++ tests_1/test_task_3.py
@@ -0,0 +1,30 @@
+import subprocess
+import task_3
+
+
+def test_console_output():
+    # Run the task script
+    result = subprocess.run(
+        ["python", "./homework/task_3.py"], capture_output=True, text=True
+    )
+
+    # Check the printed output
+    expected_output = (
+        f"Hello, world!\n"
+        f"The value of x is 5\n"
+        f"The first number in the list numbers is 1\n"
+        f"There are 3 numbers in the list called numbers.\n"
+    )
+    assert (
+        result.stdout == expected_output
+    ), f"Expected '{expected_output}' but got '{result.stdout}'"
+
+
+def test_message_variable():
+    message = task_3.message
+    assert message == "Greetings", f"Expected Greetings but got {message}"
+
+
+if __name__ == "__main__":
+    test_console_output()
+    test_message_variable()
