b0d9af991133fec965f7fc7ce4df93de16c4ec6f
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-07 09:29:43 -0700

    Update test_task_3.py

--- tests_3/test_task_3.py
+++ tests_3/test_task_3.py
@@ -1,12 +1,7 @@
 import pytest
 import math
-import sys
-import os
 
-# Add the parent directory to sys.path to enable import of task_2
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-import task_3
+from homework import task_3
 
 def test_import():
     try:
@@ -42,9 +37,3 @@ def test_approximate_cos():
         expected_value = math.cos(x)
         approx_value = task_3.approximate_cos(x, tol)
         assert abs(approx_value - expected_value) < tol, f"Expected approximate_cos({x}, {tol}) to be close to {expected_value}, but got {approx_value}"
-
-if __name__ == "__main__":
-    test_import()
-    test_approximate_euler_number()
-    test_approximate_ln()
-    test_approximate_cos()
